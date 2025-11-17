defc4edae0063e68c830dc8666e54a1ca0879406
isaacrvgg7 <isaacmoreirag777@gmail.com>
2025-11-17 08:23:17 -0300

    Update jogadores .js

--- jogadores .js
+++ jogadores .js
@@ -1,19 +1,19 @@
 function imagem1() {
-  document.getElementById("imagemJogador").src = "../Imagens/andrey3.png";
+  document.getElementById("imagemJogador").src = "./Imagens/andrey3.png";
 }
 
 function imagem2() {
-  document.getElementById("imagemJogador").src = "../Imagens/joaopedro2.png";
+  document.getElementById("imagemJogador").src = "./Imagens/joaopedro2.png";
 }
 
 function imagem3() {
-  document.getElementById("imagemJogador").src = "../Imagens/gabriel2.png";
+  document.getElementById("imagemJogador").src = "./Imagens/gabriel2.png";
 }
 
 function imagem4() {
-  document.getElementById("imagemJogador").src = "../Imagens/juninho2.png";
+  document.getElementById("imagemJogador").src = "./Imagens/juninho2.png";
 }
 
 function imagem5() {
-  document.getElementById("imagemJogador").src = "../Imagens/diego2.png";
+  document.getElementById("imagemJogador").src = "./Imagens/diego2.png";
 }
